31bb79a60f3bc671d62a4706ee86094e62e755d6
lcneo <674292593@qq.com>
2018-05-16 23:38:08 +0800

    git for mac

--- datacleaning.py
+++ datacleaning.py
@@ -8,13 +8,19 @@ Created on Wed May 16 21:08:15 2018
 
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
+import warnings
 
-data = pd.read_csv("./data/origindata.txt",header=None)
+warnings.filterwarnings("ignore")
 
+def loaddata():
+    data = pd.read_csv("data/origindata.txt",header=None)
+    data.columns = ['time','value']
+    return data
 
 #获取缺失块的索引范围
 def getloseindex(data):
-    loselist = list(data[data[1] == 999999].index)
+    loselist = list(data[data['value'] == 999999].index)
     start,end,lose = loselist[0],loselist[0],[]
     
     for i in loselist:
@@ -69,20 +75,44 @@ def getrfindex(lose):
     for i in lose:
         start,end = i
         size = end - start+1
-        rfindex.append(list(range(end+1-size,start-size)))
+        rfindex.append(list(range(start-size,end+1-size))[::-1])
     return rfindex
 
 
+def blockfilling(data,index,fillindex):
+    fdata = data
+    for i in tqdm(range(len(index))):
+        fdata['value'].ix[index[i]] = fdata['value'].ix[fillindex[i]]
+    return fdata
+
+
 #缺失数据块进行填充
-def filling():
-    pass
+def filling(save=False):
+    data = loaddata()
+    lose = getloseindex(data)
+    index = getindex(lose)
+    findex = getfindex(lose)
+    data = blockfilling(data,index,findex)
+    if(save == True):
+        data.to_csv("data/fillingdata.csv",index=False,encoding="utf-8")
+        print("成功写入文件")
+    return data
 
 #缺失数据块进行反向填充
-def reverse_filling():
-    pass
+def reverse_filling(save=False):
+    data = loaddata()
+    lose = getloseindex(data)
+    index = getindex(lose)
+    rfindex = getrfindex(lose)
+    data = blockfilling(data,index,rfindex)
+    if(save == True):
+        data.to_csv("data/rfillingdata.csv",index=False,encoding="utf-8")
+        print("成功写入文件")
+    return data
 
 
 if __name__ == "__main__":
     
-    lose = getloseindex(data)
-    index = getindex(lose)
+    a = filling(save=True)
+    b = reverse_filling(save=True)
+    
