01f1ba228a220b08f6f2093155ffd4d9a6142de0
lcneo <674292593@qq.com>
2018-05-17 07:10:27 +0800

    git for mac

---
+++ LSTM.m
@@ -0,0 +1,123 @@
+data = chickenpox_dataset;
+data = [data{:}];
+
+figure
+plot(data)
+xlabel("Month")
+ylabel("Cases")
+title("Monthy Cases of Chickenpox")
+
+
+
+numTimeStepsTrain = floor(0.9*numel(data));
+XTrain = data(1:numTimeStepsTrain);
+YTrain = data(2:numTimeStepsTrain+1);
+XTest = data(numTimeStepsTrain+1:end-1);
+YTest = data(numTimeStepsTrain+2:end);
+
+
+mu = mean(XTrain);
+sig = std(XTrain);
+
+XTrain = (XTrain - mu) / sig;
+YTrain = (YTrain - mu) / sig;
+
+XTest = (XTest - mu) / sig;
+
+
+
+
+inputSize = 1;
+numResponses = 1;
+numHiddenUnits = 200;
+
+layers = [ ...
+    sequenceInputLayer(inputSize)
+    lstmLayer(numHiddenUnits)
+    fullyConnectedLayer(numResponses)
+    regressionLayer];
+
+
+opts = trainingOptions('adam', ...
+    'MaxEpochs',250, ...
+    'GradientThreshold',1, ...
+    'InitialLearnRate',0.005, ...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropPeriod',125, ...
+    'LearnRateDropFactor',0.2, ...
+    'Verbose',0, ...
+    'Plots','training-progress');
+
+net = trainNetwork(XTrain,YTrain,layers,opts);
+
+net = predictAndUpdateState(net,XTrain);
+[net,YPred] = predictAndUpdateState(net,YTrain(end));
+
+numTimeStepsTest = numel(XTest);
+for i = 2:numTimeStepsTest
+    [net,YPred(1,i)] = predictAndUpdateState(net,YPred(i-1));
+end
+
+YPred = sig*YPred + mu;
+
+rmse = sqrt(mean((YPred-YTest).^2))
+
+figure
+plot(data(1:numTimeStepsTrain))
+hold on
+idx = numTimeStepsTrain:(numTimeStepsTrain+numTimeStepsTest);
+plot(idx,[data(numTimeStepsTrain) YPred],'.-')
+hold off
+xlabel("Month")
+ylabel("Cases")
+title("Forecast")
+legend(["Observed" "Forecast"])
+
+
+figure
+subplot(2,1,1)
+plot(YTest)
+hold on
+plot(YPred,'.-')
+hold off
+legend(["Observed" "Forecast"])
+ylabel("Cases")
+title("Forecast")
+
+subplot(2,1,2)
+stem(YPred - YTest)
+xlabel("Month")
+ylabel("Error")
+title("RMSE = " + rmse)
+
+
+
+net = resetState(net);
+net = predictAndUpdateState(net,XTrain);
+
+YPred = [];
+numTimeStepsTest = numel(XTest);
+for i = 1:numTimeStepsTest
+    [net,YPred(1,i)] = predictAndUpdateState(net,XTest(i));
+end
+
+
+YPred = sig*YPred + mu;
+
+rmse = sqrt(mean((YPred-YTest).^2))
+
+figure
+subplot(2,1,1)
+plot(YTest)
+hold on
+plot(YPred,'.-')
+hold off
+legend(["Observed" "Predicted"])
+ylabel("Cases")
+title("Forecast with Updates")
+
+subplot(2,1,2)
+stem(YPred - YTest)
+xlabel("Month")
+ylabel("Error")
+title("RMSE = " + rmse)
